094748e462d7e3f10d88c2b5dcf8d29ec393bf4e
killian ryan <killian_ryan@hotmail.com>
2023-04-21 13:14:47 +0100

    fix: adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,14 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-      precompress: false,
-      strict: false,
-    }),
+    adapter: adapter(),
   },
 };
 
